7218df549ee0a016dd4ad8fea37b4d14b4ff8636
ngphuocan <anquenmatkhau@gmail.com>
2025-10-08 21:06:11 +0700

    Update bt1.js

--- bt1.js
+++ bt1.js
@@ -2,12 +2,10 @@ let n, input;
 do {
   input = prompt("Nhập một số nguyên:");
   n = parseInt(input);
-
   if (isNaN(n)) {
-    alert("Bạn phải nhập một số nguyên hợp lệ!");
+    alert("Bạn phải nhập một số nguyên");
   }
 } while (isNaN(n));
-
 if (n > 0) {
   console.log("Số dương");
 } else if (n < 0) {
